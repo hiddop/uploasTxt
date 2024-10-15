9319df18c84022219ac852d05cd829c4fa31c7c4
Sethijai <148748128+Sethijai@users.noreply.github.com>
2024-10-15 08:46:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6655736303:AAHhiNLonJsr7wiCq_SWjF4St_AhdxqetyI")
-    API_ID = int(os.environ["API_ID", 21659643]
-    API_HASH = os.environ["API_HASH", "c5f065905ef47e693b47e0f60bb67217"]
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7821208337:AAG12bK7XkDuSFIgXP-2FypgjK9Y7Vq2ahw")
+    API_ID = int(os.environ["API_ID", 23713783]
+    API_HASH = os.environ["API_HASH", "2daa157943cb2d76d149c4de0b036a99"]
     AUTH_USERS = "5487643307"""
 # jisko bhi is repo ko deploy karne me error aa raha ho ( https://t.me/Stubborn1223 ) ye mere TG ka id ha message karke iska screen shot send kar dena me error solve kar dunga. 
