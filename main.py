db91109e7b721ece4ccdc9d222ea47066f9da9ee
Sethijai <148748128+Sethijai@users.noreply.github.com>
2024-10-22 14:34:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,26 +39,30 @@ async def restart_handler(_, m):
 
 @bot.on_message(filters.command('h2t'))
 async def run_bot(bot: Client, m: Message):
-    editable = await m.reply_text(" Send Your HTML file\n")
+    editable = await m.reply_text("Send Your HTML file\n")
     input: Message = await bot.listen(editable.chat.id)
     html_file = await input.download()
     await input.delete(True)
     await editable.delete()
+
     with open(html_file, 'r') as f:
-    soup = BeautifulSoup(f, 'html.parser')
-    tables = soup.find_all('table')
-    videos = []
-    for table in tables:
-    rows = table.find_all('tr')
-    for row in rows:
-    cols = row.find_all('td')
-    name = cols[0].get_text().strip()
-    link = cols[1].find('a')['href']
-    videos.append(f'{name}:{link}')
+        soup = BeautifulSoup(f, 'html.parser')
+        tables = soup.find_all('table')
+        videos = []
+
+        for table in tables:
+            rows = table.find_all('tr')
+            for row in rows:
+                cols = row.find_all('td')
+                name = cols[0].get_text().strip()
+                link = cols[1].find('a')['href']
+                videos.append(f'{name}:{link}')
+
     txt_file = os.path.splitext(html_file)[0] + '.txt'
     with open(txt_file, 'w') as f:
-    f.write('\n'.join(videos))
-    await m.reply_document(document=txt_file,caption="Here is your txt file.")
+        f.write('\n'.join(videos))
+
+    await m.reply_document(document=txt_file, caption="Here is your txt file.")
     os.remove(txt_file)
                                                         
                                                         
