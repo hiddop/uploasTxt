03e7b11de021746bada21d01b1ebaa42486f0411
Sethijai <148748128+Sethijai@users.noreply.github.com>
2024-10-23 07:42:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -236,7 +236,7 @@ async def account_login(bot: Client, m: Message):
                                                         
             elif '/master.mpd' in url:                                                        
              id =  url.split("/")[-2]                                                        
-             url =  "https://d26g5bnklkwsh4.cloudfront.net/" + id + "/master.m3u8&token=" + "{NO_BW}"                                                       
+             url =  f"https://pwapi-aaebd595f347.herokuapp.com/pw-dl/{id}/master.m3u8&token={NO_BW}"                                                       
                                                         
             name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "@").replace("*", "").replace(".", "").replace("https", "").replace("http", "").replace("NONE", "https://t.me/HIDEUC").strip()                                                        
             name = f'{OP} {MR} {name1[:60]}'                                                        
