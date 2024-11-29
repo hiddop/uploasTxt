1f92d7573c1d62c8517750acaf18c1e9b3178873
Sethijai <148748128+Sethijai@users.noreply.github.com>
2024-11-29 08:40:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-from pyrogram import Client, filters
+from pyrogram.errors.exceptions.bad_request_400 import StickerEmojiInvalid                                                        
+import requests                                                        
+import json                                                        
+import subprocess                                                        
+from pyrogram import Client, filters                   
+from pyrogram.types.messages_and_media import message                                                        
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup                                                
+from pyrogram.errors import FloodWait                                                        
+from pyromod import listen                                                        
+from pyrogram.types import Message                                                        
+from pyrogram import Client, filters                                                        
+from p_bar import progress_bar                                                        
+from subprocess import getstatusoutput                                                        
+from aiohttp import ClientSession                                                        
+import helper                                                        
+from logger import logging                                                        
+import time                                                        
+import asyncio                                                        
+from pyrogram.types import User, Message                                                        
+import sys                                                        
+import re                                                        
 import os
-import subprocess
+import tempfile
+from urllib.parse import urlparse, parse_qs
+from bs4 import BeautifulSoup
+import datetime
+import aiohttp
 
 bot = Client(
     "bot",
